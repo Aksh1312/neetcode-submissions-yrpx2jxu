057197ddaf6ca7b6f658b0f48607005ccd369f87
Aksh1312 <akshayalr2005@gmail.com>
2026-06-14 14:50:13 +0530

    Add: candy - submission-1

---
+++ Data Structures & Algorithms/candy/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int can=0,n=ratings.length;
+        int[] arr=new int[n];
+        Arrays.fill(arr,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1])arr[i]=Math.max(arr[i],arr[i-1]+1);
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i+1]<ratings[i])arr[i]=Math.max(arr[i],arr[i+1]+1);
+        }
+        for(int i:arr)can+=i;
+        return can;
+    }
+}
